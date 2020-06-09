5d50f79434b1304ce2d569a4edaff84dad8473be
huypn12 <huy.phung1292@gmail.com>
2020-06-09 23:24:06 +0200

    comparision between rational funcs and chain run

--- impl/experiment_c.py
+++ impl/experiment_c.py
@@ -1,82 +1,182 @@
-## Experiment: Parse Prism result rational functions,
-## Measure run time of evaluation
-
-
-import sys
-sys.setrecursionlimit(10**8)
-
-import timeit
-import logging
-logging.basicConfig(filename='experiment_b.log',level=logging.INFO)
-
+from bayesian_mcmc import BayesianMcmc
+from bayesian_mcmc_sim import BayesianMcmcSim
 from models.bees_model import BeesModel
 
-from bayesian_mcmc import BayesianMcmc
+import itertools
+import logging
+import timeit
+import sys
+import random
+sys.setrecursionlimit(10**8)
+
+logging.basicConfig(filename='experiment_c.log', level=logging.INFO)
+
+
+def synthesize_data():
+    logging.info('DATA SYNTHESIZING')
+    logging.info('Note that this will run only once')
+    model_files = [
+        # 3 bees
+        ('models/prism_utils/bee_multiparam_synchronous_3.pm',
+         'models/prism_utils/bee_multiparam_synchronous_3.txt'),
+        # 5 bees
+        ('models/prism_utils/bee_multiparam_synchronous_5.pm',
+         'models/prism_utils/bee_multiparam_synchronous_5.txt'),
+        # 10 bees
+        ('models/prism_utils/bee_multiparam_synchronous_10.pm',
+         'models/prism_utils/bee_multiparam_synchronous_10.txt'),
+        # 10 bees
+        ('models/prism_utils/bee_multiparam_synchronous_15.pm',
+         'models/prism_utils/bee_multiparam_synchronous_15.txt')
+    ]
+
 
 def summarize_data(sample: (list, list, list)):
     (s, m, f) = sample
     logging.info("Data multinomial: {}".format(m))
     logging.info("Data histogram: {}".format(f))
 
-def summarize_inference_result(model):
-    logging.info('MCMC chain length: {}'.format(model.mh_params['chain_length']))
-    logging.info('Estimated parameter: {}'.format(model.estimated_params['P']))
-    logging.info('Highest posterior density interval: {}'.format(model.estimated_params['HPD']))
-    logging.info('Log likelihood: {}'.format(model.estimated_params['log_llh']))
-    logging.info('AIC: {}'.format(model.estimated_params['AIC']))
 
-def do_experiment(model, p_true, chain_samples):
-    # pass model in to get BSCC parameterized functions
-    logging.info('\n\n{}'.format('#'* 80))
-    logging.info('Model: {}'.format(model.get_name()))
-    logging.info('True parameters: {}'.format(p_true))
-    (s, m, f) = model.sample(params=p_true, trials_count=10000)
-    summarize_data((s, m, f))
+def summarize_inference_result(mcmc):
+    logging.info('MCMC proposed prior: Beta(alpha={},beta={})'.format(
+        mcmc.hyperparams['alpha'],
+        mcmc.hyperparams['beta']
+    ))
+    logging.info('MCMC chain length: {}'.format(
+        mcmc.mh_params['chain_length']))
+    logging.info('Estimated parameter: {}'.format(mcmc.estimated_params['P']))
+    logging.info('Highest posterior density interval: {}'.format(
+        mcmc.estimated_params['HPD']))
+    logging.info('Loglikelihood: {}'.format(
+        mcmc.estimated_params['log_llh']))
+    logging.info('AIC: {}'.format(mcmc.estimated_params['AIC']))
+
+
+def do_experiment_rational(model, data_m, mh_chain_length):
+    logging.info('Metropolis Hastings, get BSCC by rational function evaluation')
     mcmc = BayesianMcmc(model)
+    mcmc.mh_params['chain_length'] = mh_chain_length
     start_time = timeit.default_timer()
-    mcmc.estimate_p(m)
+    mcmc.estimate_p(data_m)
     stop_time = timeit.default_timer()
     elapsed_time = stop_time - start_time
     logging.info('Finished in {} seconds'.format(elapsed_time))
     summarize_inference_result(mcmc)
 
-def rand_increasing_sequence():
-    pass
 
-def mm_3params():
+def do_experiment_chainrun(model, data_m, mh_chain_length, mh_chain_run_factor):
+    logging.info('Metropolis Hastings, get BSCC stats by chain running')
+    mcmc = BayesianMcmcSim(model)
+    mcmc.mh_params['chain_length'] = mh_chain_length
+    mcmc.max_trials = mh_chain_run_factor * model.get_bscc_count()
+    start_time = timeit.default_timer()
+    mcmc.estimate_p(data_m)
+    stop_time = timeit.default_timer()
+    elapsed_time = stop_time - start_time
+    logging.info('Finished in {} seconds'.format(elapsed_time))
+    logging.info('#(chain runs) per one estimation: {}'.format(mcmc.max_trials))
+    summarize_inference_result(mcmc)
+
+
+def rand_increasing_sequence(dim):
+    seq = [random.randrange(1000000) for i in range(0, dim)]
+    norm = sum(seq) * 1.0
+    seq = [s / norm for s in seq]
+    return sorted(seq)
+
+
+def do_experiment(model, p_true, trials_count, mh_chain_length, mh_chain_run_factor):
+    logging.info('True parameters: {}'.format(p_true))
+    (s, m, f) = model.sample(params=p_true, trials_count=trials_count)
+    summarize_data((s, m, f))
+    do_experiment_rational(model, m, mh_chain_length)
+    do_experiment_chainrun(model, m, mh_chain_length, mh_chain_run_factor)
+
+
+def test_mm3params():
+    logging.info('WARM UP TEST, 3 BEES')
     dtmc_filepath = 'models/prism_utils/bee_multiparam_synchronous_3.pm'
     bscc_filepath = 'models/prism_utils/bee_multiparam_synchronous_3.txt'
     model = BeesModel.from_files(dtmc_filepath, bscc_filepath)
-    p_true = [0.1, 0.2, 0.3]
-    do_experiment(model, p_true)  
+    p_true = rand_increasing_sequence(model.get_params_count())
+    do_experiment(model, p_true, 1000, 500, 100)
 
-def mm_5params():
-    dtmc_filepath = 'models/prism_utils/bee_multiparam_synchronous_5.pm'
-    bscc_filepath = 'models/prism_utils/bee_multiparam_synchronous_5.txt'
-    model = BeesModel.from_files(dtmc_filepath, bscc_filepath)
-    p_true = [0.1, 0.2, 0.3]
-    do_experiment(model, p_true)
 
-def mm_10params():
-    dtmc_filepath = 'models/prism_utils/bee_multiparam_synchronous_10.pm'
-    bscc_filepath = 'models/prism_utils/bee_multiparam_synchronous_10.txt'
-    model = BeesModel.from_files(dtmc_filepath, bscc_filepath)
-    p_true = [0.1, 0.2, 0.3]
-    do_experiment(model, p_true)  
-
-def mm_15params():
+def test_mm15params():
+    logging.info('WARM UP TEST, 3 BEES')
     dtmc_filepath = 'models/prism_utils/bee_multiparam_synchronous_15.pm'
     bscc_filepath = 'models/prism_utils/bee_multiparam_synchronous_15.txt'
     model = BeesModel.from_files(dtmc_filepath, bscc_filepath)
-    p_true = [0.1, 0.2, 0.3]
-    do_experiment(model, p_true)  
+    p_true = rand_increasing_sequence(model.get_params_count())
+    do_experiment(model, p_true, 1000, 500, 100)
+
+
+
+def small_batch():
+    logging.info('SMALL BATCH, TEST FOR CORRECT MODEL PROCESSING')
+    model_files = [
+        # 3 bees
+        ('models/prism_utils/bee_multiparam_synchronous_3.pm',
+         'models/prism_utils/bee_multiparam_synchronous_3.txt'),
+        # 5 bees
+        ('models/prism_utils/bee_multiparam_synchronous_5.pm',
+         'models/prism_utils/bee_multiparam_synchronous_5.txt'),
+        # 10 bees
+        ('models/prism_utils/bee_multiparam_synchronous_10.pm',
+         'models/prism_utils/bee_multiparam_synchronous_10.txt'),
+        # 10 bees
+        ('models/prism_utils/bee_multiparam_synchronous_15.pm',
+         'models/prism_utils/bee_multiparam_synchronous_15.txt')
+    ]
+    
+    mh_chain_length = [100, 200]
+    mh_chain_run_factor = [100, 200]
+    synthetic_data_trials = [100, 200]
+    for setting in itertools.product(model_files, synthetic_data_trials, mh_chain_length, mh_chain_run_factor):
+        files, trials_count, mh_chainLength, mh_chainRuns = setting
+        logging.info('{}'.format('-' * 80))
+        logging.info('Model DTMC file {}'.format(files[0]))
+        logging.info('Model BSCC file {}'.format(files[1]))
+        model = BeesModel.from_files(*files)
+        logging.info('#params {}, # BSCCs{}'.format(model.get_params_count(), model.get_bscc_count()))
+        true_params = rand_increasing_sequence(model.get_params_count())
+        do_experiment(model, true_params, trials_count, mh_chainLength, mh_chainRuns)
+
+
+def large_batch():
+    logging.info('LARGE BATCH, SETTINGS TEST')
+    model_files = [
+        # 3 bees
+        ('models/prism_utils/bee_multiparam_synchronous_3.pm',
+         'models/prism_utils/bee_multiparam_synchronous_3.txt'),
+        # 5 bees
+        ('models/prism_utils/bee_multiparam_synchronous_5.pm',
+         'models/prism_utils/bee_multiparam_synchronous_5.txt'),
+        # 10 bees
+        ('models/prism_utils/bee_multiparam_synchronous_10.pm',
+         'models/prism_utils/bee_multiparam_synchronous_10.txt'),
+        # 15 bees
+        ('models/prism_utils/bee_multiparam_synchronous_15.pm',
+         'models/prism_utils/bee_multiparam_synchronous_15.txt')
+    ]
+
+    mh_chain_length = [500, 5000, 50000]
+    mh_chain_run_factor = [100, 1000]
+    synthetic_data_trials = [100, 1000]
+    for setting in itertools.product(model_files, synthetic_data_trials, mh_chain_length, mh_chain_run_factor):
+        files, trials_count, mh_chainLength, mh_chainRuns = setting
+        logging.info('Model DTMC file {}'.format(files[0]))
+        logging.info('Model BSCC file {}'.format(files[1]))
+        model = BeesModel.from_files(*files)
+        logging.info('#params {}, # BSCCs{}'.format(model.get_params_count(), model.get_bscc_count()))
+        true_params = rand_increasing_sequence(model.get_params_count())
+        do_experiment(model, true_params, trials_count, mh_chainLength, mh_chainRuns)
 
 
 def main():
-    knuth_die_experiment()
-    bees_2_experiment()
-    bees_5_experiment()
-    bees_10_experiment()
+    test_mm15params()
+    # small_batch()
+
 
 if __name__ == "__main__":
     sys.exit(main())
