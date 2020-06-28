9a8e211f8e29e0545853aacf1b208effeaf698f6
huypn12 <huy.phung1292@gmail.com>
2020-06-28 21:15:16 +0200

    test bscc parser

--- tests/test_prism_bscc_parser.py
+++ tests/test_prism_bscc_parser.py
@@ -1,51 +1,60 @@
 from scripts.models.prism_utils.prism_bscc_parser import PrismBsccParser
 
+import numpy as np
+from asteval import Interpreter
 import pytest
 
 
+@pytest.fixture
+def parser():
+    parser = PrismBsccParser("data/prism/bee_multiparam_synchronous_10.txt")
+    try:
+        parser.process()
+    except Exception as ex:
+        raise ex
+    return parser
 
-def eval_bscc_ast_pfuncs(bscc_ast_pfuncs, r):
-    aeval = Interpreter()
-    aeval.symtable['r'] = r
-    return [aeval.run(f) for f in bscc_ast_pfuncs]
 
-def test_15bees():
-    parser = PrismBsccParser("models/prism/bee_multiparam_synchronous_10.txt")
-    parser.process()
+@pytest.fixture
+def sparser():
+    parser = PrismBsccParser("data/prism/synchronous_40.txt")
+    try:
+        parser.process()
+    except Exception as ex:
+        raise ex
+    return parser
+
+
+def test_10bees(parser):
     bscc_ast_pfuncs = parser.bscc_ast_pfuncs
     bscc_str_pfuncs = parser.bscc_str_pfuncs
-    print(parser.params_count)
+    for f in bscc_str_pfuncs:
+        assert f is not None
+        assert len(f) != 0
     r = [
         0.01, 0.02, 0.03, 0.04, 0.05,
         0.06, 0.07, 0.08, 0.09, 0.1,
         0.11, 0.12, 0.13, 0.14, 0.15
     ]
-    eval_bscc = eval_bscc_ast_pfuncs(bscc_ast_pfuncs, r)
-    print(eval_bscc)
+    aeval = Interpreter()
+    aeval.symtable['r'] = r
+    eval_bscc = [aeval.run(f) for f in bscc_ast_pfuncs]
+    assert not np.isnan(np.sum(eval_bscc))
 
-def test_stress():
-    parser = PrismBsccParser("synchronous_40.txt")
-    try:
-        parser.process()
-    except Exception as ex:
-        raise ex
-    bscc_ast_pfuncs = parser.bscc_ast_pfuncs
-    bscc_str_pfuncs = parser.bscc_str_pfuncs
-    print(bscc_str_pfuncs[0])
+
+def test_stress(sparser):
+    bscc_ast_pfuncs = sparser.bscc_ast_pfuncs
+    bscc_str_pfuncs = sparser.bscc_str_pfuncs
+    for f in bscc_str_pfuncs:
+        assert f is not None
+        assert len(f) != 0
     aeval = Interpreter()
     aeval.symtable['p'] = 0.3
     aeval.symtable['q'] = 0.7
     for i in range(0, len(bscc_ast_pfuncs)):
         try:
-            print(aeval.run(bscc_ast_pfuncs[i]))
+            val = aeval.run(bscc_ast_pfuncs[i])
         except Exception as ex:
-            print(i)
-
-
-def main():
-    test_15bees()
-    #   test_stress()
-
-if __name__ == "__main__":
-    sys.exit(main())
+            raise ex
+        assert not np.isnan(val)
 
