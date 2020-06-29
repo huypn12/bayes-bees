9da0d8f016f98c59a6c754f14794e0d5d0fb9043
huypn12 <huy.phung1292@gmail.com>
2020-06-29 17:00:33 +0200

    remove junk files

--- tests/test_asteval.py
+++ tests/test_asteval.py
@@ -1,23 +1,17 @@
+from scripts.models.prism_utils.prism_bscc_parser import DeepRecursionCtx
+
 import pytest
-from asteval import Interpreter
+
 import sys
+import math
+from asteval import Interpreter
 
 
-class DeepRecursionCtx():
-    kExtendedLimit = 10**9
-    def __init__(self, ):
-        self.limit = kExtendedLimit
-        self.old_limit = sys.getrecursionlimit()
-
-    def __enter__(self):
-        sys.setrecursionlimit(self.limit)
-
-    def __exit__(self, type, value, tb):
-        sys.setrecursionlimit(self.old_limit)
-
-def test_eval(expr_str):
+def test_eval():
     aeval = Interpreter()
-    expr_str = "r[0]**4 * r[1]**2" + r[2]
-    with DeepRecursionCtx:
-        
-         
+    expr_str = "r[0]**4 * r[1]**2 +" * 10000 + '2*r[2]'
+    with DeepRecursionCtx():
+        expr = aeval.parse(expr_str)
+        aeval.symtable['r'] = [1, 2, 3]
+        res = aeval.eval(expr)
+        assert not math.isnan(res)
--- tests/test_prism_bscc_parser.py
+++ tests/test_prism_bscc_parser.py
@@ -7,54 +7,21 @@ import pytest
 
 @pytest.fixture
 def parser():
-    parser = PrismBsccParser("data/prism/bee_multiparam_synchronous_10.txt")
-    try:
-        parser.process()
-    except Exception as ex:
-        raise ex
+    parser = PrismBsccParser("data/prism/bee_multiparam_synchronous_3.txt")
     return parser
 
 
-@pytest.fixture
-def sparser():
-    parser = PrismBsccParser("data/prism/synchronous_40.txt")
-    try:
-        parser.process()
-    except Exception as ex:
-        raise ex
-    return parser
-
-
-def test_10bees(parser):
+def test_bscc_eval(parser):
+    parser.process()
     bscc_ast_pfuncs = parser.bscc_ast_pfuncs
     bscc_str_pfuncs = parser.bscc_str_pfuncs
+    assert len(bscc_ast_pfuncs) != 0
+    assert len(bscc_str_pfuncs) != 0
     for f in bscc_str_pfuncs:
         assert f is not None
         assert len(f) != 0
-    r = [
-        0.01, 0.02, 0.03, 0.04, 0.05,
-        0.06, 0.07, 0.08, 0.09, 0.1,
-        0.11, 0.12, 0.13, 0.14, 0.15
-    ]
+    r = [0.11, 0.22, 0.33]
     aeval = Interpreter()
     aeval.symtable['r'] = r
     eval_bscc = [aeval.run(f) for f in bscc_ast_pfuncs]
     assert not np.isnan(np.sum(eval_bscc))
-
-
-def test_stress(sparser):
-    bscc_ast_pfuncs = sparser.bscc_ast_pfuncs
-    bscc_str_pfuncs = sparser.bscc_str_pfuncs
-    for f in bscc_str_pfuncs:
-        assert f is not None
-        assert len(f) != 0
-    aeval = Interpreter()
-    aeval.symtable['p'] = 0.3
-    aeval.symtable['q'] = 0.7
-    for i in range(0, len(bscc_ast_pfuncs)):
-        try:
-            val = aeval.run(bscc_ast_pfuncs[i])
-        except Exception as ex:
-            raise ex
-        assert not np.isnan(val)
-
